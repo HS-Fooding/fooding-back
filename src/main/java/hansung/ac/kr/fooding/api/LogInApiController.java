345ab397f4bbf93c35eb4ace8983cd456ba72e12
HyunSeong Jeong <chocomilk8604@gmail.com>
2022-03-22 08:31:45 +0900

    스웨거 정상동작 확인함. 사용법은 추가로 알아봐야 함

--- src/main/java/hansung/ac/kr/fooding/api/LogInApiController.java
+++ src/main/java/hansung/ac/kr/fooding/api/LogInApiController.java
@@ -5,6 +5,10 @@ import hansung.ac.kr.fooding.exception.X_IdAlreadyExistsException;
 import hansung.ac.kr.fooding.exception.X_NickNameAlreadyExistsException;
 import hansung.ac.kr.fooding.repository.MemberRepository;
 import hansung.ac.kr.fooding.service.MemberService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "LogInApiController", description = "로그인 및 회원가입 API")
 @RestController
 @RequiredArgsConstructor
 public class LogInApiController {
@@ -20,6 +25,18 @@ public class LogInApiController {
     private final MemberRepository memberRepository;
     private final MemberService memberService;
 
+    @ApiOperation(value="회원 가입")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value="아이디", required=true, dataType = "String", paramType="path"),
+            @ApiImplicitParam(name = "password", value="패스워드", required=true, dataType = "String", paramType="path"),
+            @ApiImplicitParam(name = "name", value="이름", required=true, dataType = "String", paramType="path"),
+            @ApiImplicitParam(name = "nickName", value="닉네임", required=true, dataType = "String", paramType="path"),
+            @ApiImplicitParam(name = "sex", value="성별", required=true, dataType = "boolean", paramType="path"),
+            @ApiImplicitParam(name = "birthDate", value="생일", required=true, dataType = "String", paramType="path"),
+            @ApiImplicitParam(name = "favor", value="좋아하는 것", required=true, dataType = "List<Enum>", paramType="path"),
+            @ApiImplicitParam(name = "role", value="권한", required=true, dataType = "List<String>", paramType="path"),
+            @ApiImplicitParam(name = "job", value="직업", required=true, dataType = "Enum", paramType="path")
+    })
     @PostMapping("/join")
     public ResponseEntity join(@RequestBody JoinReqDTO request) throws
             X_IdAlreadyExistsException,
---
+++ src/main/java/hansung/ac/kr/fooding/configuration/SwaggerConfiguration.java
@@ -0,0 +1,49 @@
+package hansung.ac.kr.fooding.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableAsync
+@EnableWebMvc
+@EnableSwagger2
+public class SwaggerConfiguration implements WebMvcConfigurer {
+    private static final String API_NAME = "Test API";
+    private static final String API_VERSION = "0.0.1";
+    private static final String API_DESCRIPTION = "Test API 명세서";
+
+    @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/"); }
+
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("hansung.ac.kr.fooding.controller"))
+                .paths(PathSelectors.any())
+                .build(); }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
+                .build();
+    }
+
+}
