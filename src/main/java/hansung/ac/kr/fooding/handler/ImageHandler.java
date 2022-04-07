40ac39b31b5a70006524ce2a4e5143815b08fc1e
HubCreator <chocomilk8604@gmail.com>
2022-04-07 13:36:54 +0900

    .gitignore 원복

---
+++ src/main/java/hansung/ac/kr/fooding/handler/ImageHandler.java
@@ -0,0 +1,93 @@
+package hansung.ac.kr.fooding.handler;
+
+import hansung.ac.kr.fooding.domain.image.Image;
+import hansung.ac.kr.fooding.var.Variable;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ImageHandler {
+    private static final String urlPath = "http://13.124.207.219:8080/fooding/image/";
+    public static List<Image> upload(List<MultipartFile> multipartImages){
+        List<Image> resultImages = new ArrayList<Image>();
+        if(multipartImages == null)
+            return null;
+        for(MultipartFile multipartImage : multipartImages) {
+            String path = getRootFolder();
+            String fileName = getNowTime24()  + "_" +multipartImage.getOriginalFilename();
+
+            try {
+                multipartImage.transferTo(new File(path+fileName));
+                String imagePath = urlPath+fileName;
+                Image resultImage = new Image(imagePath);
+                resultImages.add(resultImage);
+            } catch (IllegalStateException | IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        return resultImages;
+    }
+
+    public static Image upload(MultipartFile multipartImage){
+        if (multipartImage == null) return null;
+        String path = getRootFolder();
+        String fileName = getNowTime24()  + "_" +multipartImage.getOriginalFilename();
+
+        try {
+            multipartImage.transferTo(new File(path+fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        String imagePath = urlPath+fileName;
+        return new Image(imagePath);
+    }
+
+    private static String getRootFolder() throws IllegalStateException{
+        String os = System.getProperty("os.name").toLowerCase();
+        String rootFolder = "";
+        if (os.contains("win")) {
+            System.out.println("\n");
+            System.out.println("=======================================");
+            System.out.println("[ModuleApiController] : [resourceInsertImage] : [os check]");
+            System.out.println("[os] : " + "Windows");
+            System.out.println("=======================================");
+            System.out.println("\n");
+//            rootFolder = Variable.LOCAL_FILE_PATH; //윈도우 경로 (디스크 필요)
+            rootFolder = Variable.LOCAL_FILE_PATH_JHS; //윈도우 경로 (디스크 필요)
+
+
+        } else if (os.contains("linux")) {
+            System.out.println("\n");
+            System.out.println("=======================================");
+            System.out.println("[ModuleApiController] : [resourceInsertImage] : [os check]");
+            System.out.println("[os] : " + "Linux");
+            System.out.println("=======================================");
+            System.out.println("\n");
+            rootFolder = Variable.SERVER_FILE_PATH;
+        } else {
+            System.out.println("\n");
+            System.out.println("=======================================");
+            System.out.println("[ModuleApiController] : [resourceInsertImage] : [os check]");
+            System.out.println("[os] : " + "None");
+            System.out.println("=======================================");
+            System.out.println("\n");
+            throw new IllegalStateException("Can't decide Image Root Path");
+        }
+        return rootFolder;
+    }
+
+    private static String getNowTime24() {
+        long time = System.currentTimeMillis();
+        //SimpleDateFormat dayTime = new SimpleDateFormat("hh:mm:ss");
+        SimpleDateFormat dayTime = new SimpleDateFormat("yyyyMMddkkmmss");
+        String str = dayTime.format(new Date(time));
+        return "PT"+str; //TODO [PT는 picture 의미]
+    }
+}
---
+++ src/main/java/hansung/ac/kr/fooding/var/Variable.java
@@ -0,0 +1,8 @@
+package hansung.ac.kr.fooding.var;
+
+public class Variable {
+        public final static String KAKAO_KEY = "KakaoAK fa1ee99a854e60920df76aee916880e5";
+        public final static String LOCAL_FILE_PATH = "D://dev/images/";
+        public final static String LOCAL_FILE_PATH_JHS = "C://";
+        public final static String SERVER_FILE_PATH = "/capstone/sample_project_jhs/";
+}
