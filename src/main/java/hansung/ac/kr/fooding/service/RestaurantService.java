94c6e13bfd611bfbda1af240a50a1828c76c6444
HubCreator <chocomilk8604@gmail.com>
2022-05-18 12:51:14 +0900

    반경 수정

--- src/main/java/hansung/ac/kr/fooding/service/RestaurantService.java
+++ src/main/java/hansung/ac/kr/fooding/service/RestaurantService.java
@@ -139,7 +139,7 @@ public class RestaurantService {
         // x, y 좌표를 중점으로하고, 특정 반경 내에 위치하는 매장들의 리스트들을 반환
 //        Float r = 0.0037f; // "한성대"와 "한성대역 입구" 사이의 직선거리의 절반 거리
 
-        Float r = 0.3f; // 300m 정도인가..? 확인 필요
+        Float r = 0.9f; // 0.3(약 300m)에서 0.9로 수정함, 확인 필요
 
         Slice<Restaurant> restaurants = restaurantRepository.findRestByCoord((double)x, (double)y, (double)r, pageable);
         return restaurants.map(RestSimpleGetWithLocDTO::from);
